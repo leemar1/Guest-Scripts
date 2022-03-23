465a99f084939534ae65306706ea1afa9baf6bfa
leemar1 <41431964+leemar1@users.noreply.github.com>
2022-03-23 14:40:01 +0000

    Update Get-GuestsCSV.ps1
    Modified to include the detection of AzureAD Powershell Module

--- Get-GuestsCSV.ps1
+++ Get-GuestsCSV.ps1
@@ -106,8 +106,25 @@ $exportfile = ".\GuestUsers-", (get-date -Format 'yyMMddHHmm'), ".csv" -join ""
 $message = "Output File Name =", $exportfile -join " "
 Write-Log $message info ScriptBody
 
-Install-Module -Name AzureADPreview
-Connect-AzureAD
+Write-Host "Checking for AzureAD module..."
+
+    $AadModule = Get-Module -Name "AzureAD" -ListAvailable
+
+    if ($AadModule -eq $null) {
+
+        Write-Host "AzureAD PowerShell module not found, looking for AzureADPreview"
+        $AadModule = Get-Module -Name "AzureADPreview" -ListAvailable
+
+    }
+
+    if ($AadModule -eq $null) {
+        write-host
+        write-host "AzureAD Powershell module not installed..." -f Red
+        write-host "Install by running 'Install-Module AzureAD' or 'Install-Module AzureADPreview' from an elevated PowerShell prompt" -f Yellow
+        write-host "Script can't continue..." -f Red
+        write-host
+        exit
+    }
 
 $guests = get-azureaduser | Where-Object {$_.UserType -eq 'guest'}
 
